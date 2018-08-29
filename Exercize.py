21a1f5d822df3a8c729a57e4b7235f4b9950dae6
Konstantin <kapurelloruru@gmail.com>
2018-08-29 16:46:45 +0300

    Добавление первых файлов :)

---
+++ Exercize.py
@@ -0,0 +1,14 @@
+def get_vat(payment, percent=18):
+    try:
+    	print(percent)
+    	payment = float(payment)
+    	percent = float(percent)
+    	vat = payment / 100 * percent
+    	vat = round(vat, 2)
+    	return "Сумма НДС: {}".format(vat)
+    except (TypeError, ValueError):
+    	return "Проверьте правильность ввода"
+
+
+result = get_vat(570, "10")
+print (result)
